942963751f0139a0eeec87481e6575262f37d07f
Thiago Silva <thyagosousasilva@gmail.com>
2021-06-08 08:12:33 -0300

    Creating jwt user helper class

--- backend/src/main/java/com/thiago/backend/api/security/jwt/JwtUserFactory.java
+++ backend/src/main/java/com/thiago/backend/api/security/jwt/JwtUserFactory.java
@@ -1,5 +1,36 @@
 package com.thiago.backend.api.security.jwt;
 
-public class JwtUserFactory {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.thiago.backend.api.entity.User;
+import com.thiago.backend.api.enums.ProfileEnum;
+
+/*
+ * Classe Factory que converte um usuário (User), para um tipo reconhecível
+ * pelo SpringSecurity
+ * 
+ * */
+public class JwtUserFactory {
+	private JwtUserFactory() {
+		
+	}
+	
+	/*
+	 * gera um JwtUser com base nos dados de um User, dessa forma, convertendo o
+	 * perfil do usuário para o formato usado pelo SpringSecurity
+	 * 
+	 * */
+	public static JwtUser create(User user) {
+		return new JwtUser(user.getId(), user.getEmail(), user.getPassword(), mapToGrantedAuthorities(user.getProfile()));
+	}
+	
+	private static List<GrantedAuthority> mapToGrantedAuthorities(ProfileEnum profileEnum){
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority(profileEnum.toString()));
+		return authorities;
+	}
 }
