20796afca204cff3a2f11648e8d9616214a71a97
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-16 22:42:15 -0300

    Finalizando UserController

--- backend/src/main/java/com/thiago/backend/api/controller/UserController.java
+++ backend/src/main/java/com/thiago/backend/api/controller/UserController.java
@@ -8,13 +8,18 @@ import com.thiago.backend.api.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,5 +80,84 @@ public class UserController {
         if (user.getEmail() == null) {
             result.addError(new ObjectError("User email", "E-mail não pode ser vazio"));
         }
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAnyRole('ADMIN')") // Somente o administrador do sistema pode usar este método
+    public ResponseEntity<Response<User>> update(HttpServletRequest request, //
+            @RequestBody User user, // Dados que serão enviados pela requisição
+            BindingResult result // se houver algum erro, será passado por aqui nas validações
+    ) {
+        Response<User> response = new Response<User>();
+        try {
+
+            validateUpdateUser(user, result);
+            if (result.hasErrors()) {
+                result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User userPersisted = (User) userService.createOrUpdate(user);
+            response.setData(userPersisted);
+
+        } catch (Exception e) {
+            response.getErrors().add(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+    // Valida a atualização de uma entrada
+    private void validateUpdateUser(User user, BindingResult result) {
+        if (user.getId() == null) {
+            result.addError(new ObjectError("User", "campo ID não possui informação"));
+        }
+
+        if (user.getEmail() == null) {
+            result.addError(new ObjectError("User email", "E-mail não pode ser vazio"));
+        }
+    }
+
+    @GetMapping(value = "{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // Autorização de acordo com o perfil
+    public ResponseEntity<Response<User>> findById(@PathVariable("id") String id) {
+        Response<User> response = new Response<User>();
+        User user = userService.findById(id);
+
+        // Se o id pesquisado não retornar nenhum usuário associado, retorna um erro
+        if (user == null) {
+            response.getErrors().add("Usuário " + id + " não encontrado!");
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        response.setData(user);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping(value = "{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // Autorização de acordo com o perfil
+    public ResponseEntity<Response<String>> delete(@PathVariable("id") String id) {
+        Response<String> response = new Response<String>();
+        User user = userService.findById(id);
+
+        // Se o id pesquisado não retornar nenhum usuário associado, retorna um erro
+        if (user == null) {
+            response.getErrors().add("Usuário " + id + " não encontrado!");
+            return ResponseEntity.badRequest().body(response);
+        }
+        userService.delete(id);
+        response.setData("Usuário " + id + " deletado!");
+        return ResponseEntity.ok(new Response<String>());
+    }
+
+    @GetMapping(value = "{page}/{count}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // Autorização de acordo com o perfil
+    public ResponseEntity<Response<Page<User>>> findAll(@PathVariable int page, @PathVariable int count) {
+        Response<Page<User>> response = new Response<Page<User>>();
+        Page<User> users = userService.findAll(page, count);
+        response.setData(users);
+        return ResponseEntity.ok(response);
     }
 }
